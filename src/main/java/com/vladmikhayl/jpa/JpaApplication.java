119572e6387368992a702715c6bee6e8826e3179
vladmikhayl <vamikhaylov_2@edu.hse.ru>
2025-03-13 18:51:31 +0300

    added queries

--- src/main/java/com/vladmikhayl/jpa/JpaApplication.java
+++ src/main/java/com/vladmikhayl/jpa/JpaApplication.java
@@ -1,14 +1,19 @@
 package com.vladmikhayl.jpa;
 
+import com.github.javafaker.Faker;
 import com.vladmikhayl.jpa.models.Author;
 import com.vladmikhayl.jpa.models.recources.Video;
 import com.vladmikhayl.jpa.repositories.AuthorRepository;
 import com.vladmikhayl.jpa.repositories.VideoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
+@Slf4j
 @SpringBootApplication
 public class JpaApplication {
 
@@ -22,19 +27,61 @@ public class JpaApplication {
 			VideoRepository videoRepository
 	) {
 		return args -> {
-			Author author = Author.builder()
+			/*Author author = Author.builder()
 					.firstName("Vlad")
 					.lastName("Mikhaylov")
 					.age(20)
 					.email("vlad@yandex.ru")
 					.build();
-			authorRepository.save(author);
+			authorRepository.save(author);*/
 
-			Video video = Video.builder()
+			Faker faker = new Faker();
+
+			for (int i = 0; i < 50; i++) {
+				Author author = Author.builder()
+						.firstName(faker.name().firstName())
+						.lastName(faker.name().lastName())
+						.age(faker.number().numberBetween(19, 80))
+						.email(faker.name().username() + "@yandex.ru")
+						.build();
+				authorRepository.save(author);
+			}
+
+			/*Author author = Author.builder()
+					.id(1)
+					.firstName("Vlad")
+					.lastName("Mikhaylov")
+					.age(20)
+					.email("vlad@yandex.ru")
+					.build();
+			authorRepository.save(author);*/
+
+//			authorRepository.updateAuthor(22, 1);
+
+//			authorRepository.updateAllAuthorsAges(99);
+
+			/*Video video = Video.builder()
 					.name("abc")
 					.length(5)
 					.build();
-			videoRepository.save(video);
+			videoRepository.save(video);*/
+
+			/*List<Author> authorsByFirstName = authorRepository.findAllByFirstName("Billy");
+			log.info("\nfindAllByFirstName: " + authorsByFirstName.get(0).getFirstName()
+					+ " " + authorsByFirstName.get(0).getLastName());
+
+			List<Author> authorsByFirstNameIgnoreCase = authorRepository.findAllByFirstNameIgnoreCase("billy");
+			log.info("\nfindAllByFirstNameIgnoreCase: " + authorsByFirstNameIgnoreCase.get(0).getFirstName()
+					+ " " + authorsByFirstNameIgnoreCase.get(0).getLastName());
+
+			List<Author> authorsByFirstNameContainingIgnoreCase = authorRepository.findAllByFirstNameContainingIgnoreCase("nn");
+			log.info("\nfindAllByFirstNameContainingIgnoreCase: " + authorsByFirstNameContainingIgnoreCase.get(0).getFirstName()
+					+ " " + authorsByFirstNameContainingIgnoreCase.get(0).getLastName());
+
+			List<Author> authorsByFirstNameEndsWithIgnoreCase = authorRepository.findAllByFirstNameEndsWithIgnoreCase("N");
+			log.info("\nfindAllByFirstNameEndsWithIgnoreCase: " + authorsByFirstNameEndsWithIgnoreCase.get(0).getFirstName()
+					+ " " + authorsByFirstNameEndsWithIgnoreCase.get(0).getLastName());*/
+
 		};
 	}
 
--- src/main/java/com/vladmikhayl/jpa/repositories/AuthorRepository.java
+++ src/main/java/com/vladmikhayl/jpa/repositories/AuthorRepository.java
@@ -1,8 +1,35 @@
 package com.vladmikhayl.jpa.repositories;
 
 import com.vladmikhayl.jpa.models.Author;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
+
+    @Modifying
+    @Transactional
+    @Query("update Author a set a.age = :age where a.id = :id")
+    int updateAuthor(int age, int id);
+
+    @Modifying
+    @Transactional
+    @Query("update Author a set a.age = :age")
+    void updateAllAuthorsAges(int age);
+
+    List<Author> findAllByFirstName(String fn);
+
+    List<Author> findAllByFirstNameIgnoreCase(String fn);
+
+    List<Author> findAllByFirstNameContainingIgnoreCase(String fn);
+
+    List<Author> findAllByFirstNameStartsWithIgnoreCase(String fn);
+
+    List<Author> findAllByFirstNameEndsWithIgnoreCase(String fn);
+
+    List<Author> findAllByFirstNameInIgnoreCase(List<String> firstNames);
 
 }
