6ec482f61c81ae10bba054beec63a6fc06baf4e7
vladmikhayl <vamikhaylov_2@edu.hse.ru>
2025-03-13 15:16:09 +0300

    added embedding

---
+++ src/main/java/com/vladmikhayl/jpa/models/embedded/OrderId.java
@@ -0,0 +1,20 @@
+package com.vladmikhayl.jpa.models.embedded;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Embeddable
+public class OrderId implements Serializable {
+
+    private String username;
+    private LocalDateTime orderDate;
+
+}
